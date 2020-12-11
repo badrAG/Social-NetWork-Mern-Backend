f3cc5a626eb07265fd87b1309a9f4ac59dce5325
unknown <badrag6@gmail.com>
2020-12-11 02:50:46 +0100

    up

--- controllers/post.js
+++ controllers/post.js
@@ -1,122 +1,133 @@
 const Post = require("../models/post");
-const getAllPosts = (req,res)=>{
-    let following = req.profile.following;
-    following.push(req.profile._id);
-    Post.find({PostedBy:{$in : following}})
-    .populate("comments","text created")
-    .populate("comments.commentedBy","_id UserName")
-    .populate("PostedBy","_id UserName")
+const getAllPosts = (req, res) => {
+  let following = req.profile.following;
+  following.push(req.profile._id);
+  Post.find({ PostedBy: { $in: following } })
+    .populate("comments", "text created")
+    .populate("comments.commentedBy", "_id UserName")
+    .populate("PostedBy", "_id UserName")
     .sort("-createdAt")
-    .exec((err,posts)=>{
-        if(err) res.json({error:err});
-        res.json(posts);
+    .exec((err, posts) => {
+      if (err) res.json({ error: err });
+      res.json(posts);
     });
 };
 
-const getUserPosts = (req,res)=>{
-    Post.find({PostedBy:req.profile._id})
-    .populate("comments","text created")
-    .populate("comments.commentedBy","_id UserName")
-    .populate("PostedBy","_id UserName")
+const getUserPosts = (req, res) => {
+  Post.find({ PostedBy: req.profile._id })
+    .populate("comments", "text created")
+    .populate("comments.commentedBy", "_id UserName")
+    .populate("PostedBy", "_id UserName")
     .sort("-createdAt")
-    .exec((err,posts)=>{
-        if(err) res.json({error:err});
-        res.json(posts);
+    .exec((err, posts) => {
+      if (err) res.json({ error: err });
+      res.json(posts);
     });
 };
 
-const getPostById = (req,res,next,id)=>{
-    Post.findById(id)
-    .populate("comments","text created")
-    .populate("comments.commentedBy","_id UserName")
-    .populate("PostedBy","_id UserName")
-    .exec((err,post)=>{
-        if(err) res.json({error:err});
-        req.post = post;
-        next();
+const getPostById = (req, res, next, id) => {
+  Post.findById(id)
+    .populate("comments", "text created")
+    .populate("comments.commentedBy", "_id UserName")
+    .populate("PostedBy", "_id UserName")
+    .exec((err, post) => {
+      if (err) res.json({ error: err });
+      req.post = post;
+      next();
     });
 };
 
-const isOwner = (req,res,next)=>{
-    let isMatch = req.post && req.auth && req.post.PostedBy._id == req.auth._id
-    if(!isMatch){
-        return res.json({error:"Not Authorized"});
-    }
-    next();
-} 
-
-const addPost = (req,res)=>{
-    const {text} = req.body;
-    let post = new Post({text,PostedBy:req.profile._id});
-    post.save((err,data)=>{
-        if(err) res.json({error:err});
-        res.json(data);
-    });
+const isOwner = (req, res, next) => {
+  let isMatch = req.post && req.auth && req.post.PostedBy._id == req.auth._id;
+  if (!isMatch) {
+    return res.json({ error: "Not Authorized" });
+  }
+  next();
 };
 
-const deletPost = (req,res)=>{
-    let postToDelet = req.post;
-    postToDelet.remove((err,deletPost)=>{
-        if(err) res.json({error:err});
-        res.json(deletPost);
-    });
+const addPost = (req, res) => {
+  const { text } = req.body;
+  let post = new Post({ text, PostedBy: req.profile._id });
+  post.save((err, data) => {
+    if (err) res.json({ error: err });
+    res.json(data);
+  });
 };
 
-const likePost = (req,res)=>{
-    Post.findByIdAndUpdate(req.body.postId,
-        {$push:{likes:req.body.userId}},{new:true})
-    .exec((err,result)=>{
-        if(err) res.json({error : err});
-        res.json(result);
-    });
+const deletPost = (req, res) => {
+  let postToDelet = req.post;
+  postToDelet.remove((err, deletPost) => {
+    if (err) res.json({ error: err });
+    res.json(deletPost);
+  });
 };
 
-const unLikePost = (req,res)=>{
-    Post.findByIdAndUpdate(req.body.postId,
-        {$pull:{likes:req.body.userId}},
-        {new:true})
-    .exec((err,result)=>{
-        if(err) res.json({error : err});
-        res.json(result);
-    });
+const likePost = (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $push: { likes: req.body.userId } },
+    { new: true }
+  ).exec((err, result) => {
+    if (err) res.json({ error: err });
+    res.json(result);
+  });
 };
 
-const addComment = (req,res)=>{
-    let comment = {text: req.body.text};
-    comment.commentedBy = req.body.userId; 
-    Post.findByIdAndUpdate(req.body.postId,
-        {$push:{comments:comment}},
-        {new:true})
-        .populate("comments","text created")
-    .populate("comments.commentedBy","_id UserName")
-    .populate("PostedBy","_id UserName")
-    .sort("-createdAt")
-    .exec((err,result)=>{
-        if(err) res.json({error : err});
-        res.json(result);
-    });
+const unLikePost = (req, res) => {
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $pull: { likes: req.body.userId } },
+    { new: true }
+  ).exec((err, result) => {
+    if (err) res.json({ error: err });
+    res.json(result);
+  });
 };
 
-const deleteComment = (req,res)=>{
-    let commentId = req.body.commentId;
-    Post.findByIdAndUpdate(req.body.postId,
-        {$pull:{comments:{_id : commentId}}},
-        {new:true})
-    .exec((err,result)=>{
-        if(err) res.json({error : err});
-        res.json(result);
+const addComment = (req, res) => {
+  let comment = { text: req.body.text };
+  comment.commentedBy = req.body.userId;
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $push: { comments: comment } },
+    { new: true }
+  )
+  .populate("comments", "text created")
+    .populate("comments.commentedBy", "_id UserName")
+    .populate("PostedBy", "_id UserName")
+  .exec((err, result) => {
+    if (err) res.json({ error: err });
+    res.json(result);
+  });
+};
+
+const deleteComment = (req, res) => {
+    
+  let commentId = req.body.commentId;
+  console.log(commentId,"--",req.body.postId)
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    { $pull: { comments: { _id: commentId } } },
+    { new: true }
+  )
+  .populate("comments", "text created")
+    .populate("comments.commentedBy", "_id UserName")
+    .populate("PostedBy", "_id UserName")
+    .exec((err, result) => {
+      if (err) res.json({ error: err });
+      res.json(result);
     });
 };
 
 module.exports = {
-    getAllPosts,
-    addPost,
-    getUserPosts,
-    getPostById,
-    isOwner,
-    deletPost,
-    likePost,
-    unLikePost,
-    addComment,
-    deleteComment
-}
+  getAllPosts,
+  addPost,
+  getUserPosts,
+  getPostById,
+  isOwner,
+  deletPost,
+  likePost,
+  unLikePost,
+  addComment,
+  deleteComment,
+};
